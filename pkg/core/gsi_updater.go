4cfa1eacc6b6f56977f5b90b532597cca41f3c5a
blowa <light06120602@gmail.com>
2025-12-11 23:07:28 +0900

    gsi_updater.go の更新

--- pkg/core/gsi_updater.go
+++ pkg/core/gsi_updater.go
@@ -1,62 +1,86 @@
 package core
 
 import (
-    "fmt"
-        "github.com/syndtr/goleveldb/leveldb"
-        "Emulator-fr-virtuelle-Datenbanken-gobes/pkg/model"
+	"fmt"
+	"Emulator-fr-virtuelle-Datenbanken-gobes/pkg/model"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 func UpdateGSI(batch *leveldb.Batch, schema model.TableSchema, oldRecord model.Record, newRecord model.Record) {
-        if len(schema.GSIs) == 0 {
-                return
-        }
+	if len(schema.GSIs) == 0 {
+		return
+	}
 
-        for _, gsiSchema := range schema.GSIs {
-                oldPKVal, oldSKVal, oldExists := getGSIKeyValues(oldRecord, gsiSchema)
-                newPKVal, newSKVal, newExists := getGSIKeyValues(newRecord, gsiSchema)
+	for _, gsiSchema := range schema.GSIs {
+		oldPKVal, oldSKVal, oldGSIExists := getGSIKeyValues(oldRecord, gsiSchema)
+		newPKVal, newSKVal, newGSIExists := getGSIKeyValues(newRecord, gsiSchema)
+		
+		var basePKVal string
+		
+		
+		
+		if newRecord != nil {
+			if basePKAV, ok := newRecord[schema.PartitionKey]; ok {
+				basePKVal, _ = model.GetAttributeValueString(basePKAV)
+			}
+		} else if oldRecord != nil {
+			if basePKAV, ok := oldRecord[schema.PartitionKey]; ok {
+				basePKVal, _ = model.GetAttributeValueString(basePKAV)
+			}
+		} else {
+			continue 
+		}
 
-                if oldExists {
-            basePKAV, _ := oldRecord[schema.PartitionKey]
-            basePKVal, _ := model.GetAttributeValueString(basePKAV)
+		
+		if oldGSIExists {
+			
+			if !newGSIExists || oldPKVal != newPKVal || oldSKVal != newSKVal {
+				oldGSIKey := model.BuildGSILevelDBKey(gsiSchema.IndexName, oldPKVal, oldSKVal, basePKVal)
+				batch.Delete([]byte(oldGSIKey))
+			}
+		}
 
-                        oldGSIKey := model.BuildGSILevelDBKey(gsiSchema.IndexName, oldPKVal, oldSKVal, basePKVal)
-                        batch.Delete([]byte(oldGSIKey))
-                }
+		
+		if newGSIExists {
+			newGSIKey := model.BuildGSILevelDBKey(gsiSchema.IndexName, newPKVal, newSKVal, basePKVal)
+			
+			
+			gsiValue, err := model.MarshalRecord(newRecord)
+			if err != nil {
+				
+				
+				continue 
+			}
 
-                if newExists {
-            basePKAV, _ := newRecord[schema.PartitionKey]
-            basePKVal, _ := model.GetAttributeValueString(basePKAV)
-
-            newGSIKey := model.BuildGSILevelDBKey(gsiSchema.IndexName, newPKVal, newSKVal, basePKVal)
-
-            gsiValue := []byte("{}") 
-
-            batch.Put([]byte(newGSIKey), gsiValue)
-                }
-        }
+			batch.Put([]byte(newGSIKey), gsiValue)
+		}
+	}
 }
 
 func getGSIKeyValues(record model.Record, gsiSchema model.GsiSchema) (string, string, bool) {
-        if record == nil {
-                return "", "", false
-        }
+	if record == nil {
+		return "", "", false
+	}
 
-        pkAV, pkExists := record[gsiSchema.PartitionKey]
-        if !pkExists {
-                return "", "", false
-        }
-        pkVal, _ := model.GetAttributeValueString(pkAV)
+	pkAV, pkExists := record[gsiSchema.PartitionKey]
+	if !pkExists {
+		return "", "", false
+	}
+	pkVal, _ := model.GetAttributeValueString(pkAV)
 
-        var skVal string
-        skExists := true
-        if gsiSchema.SortKey != "" {
-                skAV, ok := record[gsiSchema.SortKey]
-                if ok {
-                        skVal, _ = model.GetAttributeValueString(skAV)
-                } else {
-                        skExists = false
-                }
-        }
+	var skVal string
+	skExists := true
+	if gsiSchema.SortKey != "" {
+		skAV, ok := record[gsiSchema.SortKey]
+		if ok {
+			skVal, _ = model.GetAttributeValueString(skAV)
+		} else {
+			
+			
+			return "", "", false
+		}
+	}
 
-        return pkVal, skVal, pkExists && (gsiSchema.SortKey == "" || skExists)
+	
+	return pkVal, skVal, true
 }
